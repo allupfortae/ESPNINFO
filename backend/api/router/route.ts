af07c98e6469088314e1cf89e383799238c893be
Proxy <avantaejacksoncontact@gmail.com>
2022-11-29 18:33:44 -0500

    Delete backend directory

--- backend/api/router/route.ts
+++
@@ -1,7 +0,0 @@
-import * as express from "express";
-const router = express.Router();
-import { runsInBackGround, getAllData } from "../controllers/controller";
-
-router.route("/espn").post(runsInBackGround).get(getAllData);
-
-export default router;
